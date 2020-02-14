6a9d6d4d1fa9716eb58042657a92843475dd5736
Gustavo Banin <gustavo.banin@hotmail.com>
2020-02-14 13:13:12 -0300

    Mensagem de boa noite bom dia

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -5,6 +5,7 @@ public class Programa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Boa Tarde");
+		System.out.println("Boa noite");
+		System.out.println("Bom dia");
 	}
-
 }
